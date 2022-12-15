b9900409a9086bf6836bc973d2a21ff660315785
shinhosuck <shinhosuck1973@gmail.com>
2022-12-15 12:29:08 -0800

    update

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from posts.views import home
+from posts.views import (
+        home,
+        post_detail
+    )
 
 
 app_name = 'posts'
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('post/detail/<int:pk>', post_detail, name='post-detail')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from posts.models import Post
+
+
 
 def home(request):
     posts = Post.objects.all()
-    # print(posts)
     context = {
         'posts': posts
     }
     return render(request, 'posts/home.html', context)
+
+def post_detail(request, pk):
+    try:
+        post = get_object_or_404(Post, pk=pk)
+        context = {
+            'post': post
+        }
+        return render(request, 'posts/post_detail.html', context)
+    except:
+        return redirect('posts:home')
+    
